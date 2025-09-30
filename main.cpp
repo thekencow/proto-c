301d9ce2c9506c199b91706f0dec2862dc94cbf4
kencow <kencow@gmail.com>
2025-09-30 10:21:43 -0400

    first commit
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
+//#include <iostream>
+//#include <stdio.h>
+#include <cstdio>
+
+
 
 int main ()
-    {
-          using std::cout;
-          using std::endl;
-          cout << "hello world" << endl;
-          return 0;
-    }
+{
+//          using std::cout;
+//          using std::endl;
+//          cout << "hello world" << endl;
+
+	using std::printf;
+	using std::scanf;
+	int num = 180;
+	char nombre[50];
+	printf("Your name?\n");
+	scanf("%49s", nombre);
+	
+	//printf("Hello MINIGW!\n");
+	printf("Hello %s\n", nombre);
+	
+	printf("Your lucky number is: %i", num);
+
+	printf(", its address is: %p\n", (void*)&num);
+
+	printf("Aint that nifty!\n");
+	return 0;
+
+}

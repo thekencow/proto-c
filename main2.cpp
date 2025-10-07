1a4ba256ee9f7507cf1c47b10bb6fb4c2e272995
kencow <kencow@gmail.com>
2025-10-07 14:57:31 -0400

    10 7 shenanigans

--- main2.cpp
+++ main2.cpp
@@ -1,5 +1,6 @@
 //g++ -o output.exe main.cpp -O2 -Wall
 #include <cstdio>
+#define oot printf
 
 int main ()
 {
@@ -7,14 +8,22 @@ int main ()
 	using std::scanf;
 	
 	char thisword[50];
-	
-	printf("Hello, World!\n");
+	int x = 10;
+	int* q = &x;
+	oot("Hello, World!\n");
 
 	scanf("%s", thisword);
 
 	thisword[2]++;
+	oot("The infallible number is %d, %s\n", x, thisword);
 
-	printf("I updated this via my ubuntu desktop, %s\n", thisword);
+	*q = 15;
+	thisword[3]--;
+	oot("That's right, %s, the infallible number is %d!\n",thisword,x);
+	thisword[0]++;
+	printf("I updated this via my work laptop, %s\n", thisword);
+	oot("Address of x:%p\n",(void*)&x);
+	oot("Whatever q is, as a p: %p\n",q);
 	
 	return 0;
 

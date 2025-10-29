07785ed295d5cef1a8056de1b2399c09ad9a75cd
kencow <kencow@gmail.com>
2025-10-29 16:58:06 -0400

    file io in 3

--- main3.c
+++ main3.c
@@ -14,6 +14,8 @@ main3.c:5:10: fatal error: cstdio: No such file or directory
 //#include <cstring> // C++, std namespace
 
 
+
+
 // 1. A *structure* – groups related data
 struct Person	{
 		char name[50]; //   string (C-style, fixed size)
@@ -22,9 +24,9 @@ struct Person	{
 };
 
 
-void print(const Person* p) // 2. Function that receives a *pointer*
+void print(const struct Person* p) // 2. Function that receives a *pointer*
 {
-	/*std::*/printf("Hi I am %s and I am %s! My favorite number is %i!\n", p->name, p->trait, p->favoriteNumber);
+	std::printf("Hi I am %s and I am %s! My favorite number is %i!\n", p->name, p->trait, p->favoriteNumber);
 };
 
 
@@ -33,10 +35,13 @@ int main ()
 {
 	using std::printf;
 //	using std::strcpy; // copy a string (need <cstring>)
-	
+	using std::fopen;
+	using std::fclose;
+
+
 	printf("Hello World!\n");
 
-	Person alice;	//create an intsance
+	Person alice = {0};	//create an intsance
 	
 	strcpy(alice.name, "Alice");
 	strcpy(alice.trait, "fake");
@@ -44,7 +49,72 @@ int main ()
 	
 	print(&alice); // pass its *address* (pointer) "send me the address"
 
+	int w = 217;
+	
+	char gg = char(w);
+	char loadName[50];
+	
+	
+	printf("Also,\n%c\n",gg);
+	
+	gg++;
+	
+	printf("Derr %c\n", gg);
 
+	for(int i = 176; i <= 195;i++)
+	{
+		printf("%c",i);
+		
+	};
+	printf("!\n");
+
+
+	Person bob = {0};	//create an intsance
+	
+	FILE* fp = fopen("namelist","r");
+		if (fp == NULL)
+	{
+		printf("eff pee didn't open. bummer\n");
+		return 1;
+	}
+	
+	if(fgets(loadName, 75, fp) == NULL)
+	{
+		printf("error reading name\n");
+		fclose(fp);
+		return 1;
+	}
+	printf("Loaded %s into memory, processing...\n", loadName);
+	//loadName[strcspn(loadName, "WARK")] = '\0'; //WARK was a problem for some raisin
+	loadName[strcspn(loadName, "X")] = '\0';
+	//explicitly, take the first instance of newline (or WARK, I guess) in the loadName array, and replace it with the null terminator character
+	//use it to mark segments of a file i guess
+	printf("Processed %s\n",loadName);
+	fclose(fp);
+	
+	strcpy(bob.name, loadName);
+	strcpy(bob.trait, "spiffy");
+	bob.favoriteNumber = gg;
+	
+	bob.name[strcspn(bob.name,",")] = '\0';
+	
+	bob.name[2]+=10;
+	
+	print(&bob);
+	
+	
+	FILE* rk = fopen("archive.txt", "w");
+	if (rk == NULL)
+	{
+		printf("error opening archive.txt. sorry.\n");
+		fclose(rk);
+		return 1;
+		};
+		
+		fprintf(rk,"Their names are: %s, %s\n", alice.name, bob.name);
+
+		
+	fclose(rk);
 	
 	return 0;
 }
